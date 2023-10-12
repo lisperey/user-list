7606e80652faab6798385b56f576bb80ca9522b1
lisperey <pilrss16@gmail.com>
2023-10-11 23:01:19 -0300

    Finalizado

--- Diretorio.md
+++ Diretorio.md
@@ -1,3 +1,7 @@
 # Tarefas
 
-##consumir API
+##1 - consumir API
+##2 - fazer rota
+##3 - fazer cards
+##4 - fazer paginação
+##5 - fazer botões de editar e excluir
--- user-list/app/Http/Controllers/userController.php
+++ user-list/app/Http/Controllers/userController.php
@@ -4,8 +4,71 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
-class userController extends Controller
+class UserController extends Controller
 {
-    //
+    public function index(Request $request)
+{
+    $api = Http::get('https://run.mocky.io/v3/ce47ee53-6531-4821-a6f6-71a188eaaee0');
+    $users = $api->json()['users'];
+
+    $page = $request->get('page', 1);
+    $perPage = 10;
+
+    $items = collect($users);
+
+    $users = new LengthAwarePaginator(
+        $items->forPage($page, $perPage),
+        $items->count(),
+        $perPage,
+        $page,
+        ['path' => $request->url(), 'query' => $request->query()]
+    );
+
+    return view('userList', compact('users'));
+}
+
+public function edit($id)
+{
+    $response = Http::get("https://run.mocky.io/v3/ce47ee53-6531-4821-a6f6-71a188eaaee0/}");
+    $users = $response->json()['users'];
+    $retorno = '';
+    foreach($users as $user){
+        if($user['id'] == $id){
+            $retorno = $user;
+            break;
+        }
+    }
+
+    if ($response->successful()) {
+        $user = $retorno;
+        return view('modalEdit', compact('user'));
+    } else {
+        return redirect()->route('users.index')->with('error', 'Usuário não encontrado.');
+    }
+}
+
+public function update(Request $request, $id)
+{
+    $response = Http::put("https://run.mocky.io/v3/ce47ee53-6531-4821-a6f6-71a188eaaee0/users/{$id}", $request->all());
+
+    if ($response->successful()) {
+        return redirect()->route('users.index')->with('success', 'Usuário atualizado com sucesso.');
+    } else {
+        return back()->withInput()->with('error', 'Erro ao atualizar o usuário.');
+    }
+}
+
+public function destroy($id)
+{
+    $response = Http::delete("https://run.mocky.io/v3/ce47ee53-6531-4821-a6f6-71a188eaaee0/users/{$id}"); 
+
+    if ($response->successful()) {
+        return redirect()->route('users.index')->with('success', 'Usuário excluído com sucesso.');
+    } else {
+        return redirect()->route('users.index')->with('error', 'Erro ao excluir o usuário.');
+    }
+}
 }
